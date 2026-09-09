75b5fec8143b715a56ebc624bc53c11ab2269b3c
MinovKJ <minovkjomon@gmail.com>
2026-09-09 14:29:16 +0530

    Enhance Launch NyxSlate.bat for instant in-folder development testing

--- Launch NyxSlate.bat
+++ Launch NyxSlate.bat
@@ -1,2 +1,9 @@
 @echo off
-start "" "%~dp0node_modules\electron\dist\electron.exe" "%~dp0."
+setlocal
+cd /d "%~dp0"
+
+if exist "%~dp0node_modules\electron\dist\NyxSlate.exe" (
+    start "" "%~dp0node_modules\electron\dist\NyxSlate.exe" "%~dp0." %*
+) else (
+    start "" "%~dp0node_modules\electron\dist\electron.exe" "%~dp0." %*
+)
